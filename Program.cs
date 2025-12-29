0a02c17966d19c4e5f298348d8ec0def94e6592c
Freddiefady <dodofady49@gmail.com>
2025-12-29 10:23:23 +0200

    Refactor Program.cs to include database migration and seeding logic

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
+builder.AddGameStoreDb();
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
 
+app.MigrateDb();
+
 app.Run();
 // run db migrations and seed data
+// first use namespaces in program.cs
+// using GameStore.Data; => to access GameStoreContext
+// builder.AddGameStoreDb(); => to seed data
+// app.MigrateDb(); => to run migrations
 // dotnet ef migrations add InitialCreate --output-dir Data/Migrations
-// dotnet ef database update --project GameStore.Data --startup-project GameStore.Api
+// dotnet ef database update
