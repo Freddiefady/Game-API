cc68905a3e0a7e6926371916edd674c752b765e8
Freddiefady <dodofady49@gmail.com>
2026-01-04 16:29:47 +0200

    FIX: Add API versioning to the service collection, not the app.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = new ApiVersion(1, 0); // Set the default API version to 1.0 if a version is not specified in the request.
+    config.AssumeDefaultVersionWhenUnspecified = true; // Assume default version when not specified
+    config.ReportApiVersions = true; // To Let clients know which API versions are supported
+    config.ApiVersionReader = new HeaderApiVersionReader("api-version"); // To pass the version information in the request header.
+});
+
+
 builder.AddGameStoreDb();
 
 var app = builder.Build();
@@ -18,14 +27,31 @@ app.MapGenresEndpoints();
 app.MigrateDb();
 
 app.UseMiddleware<ApiKeyMiddleware>();
+// Apply middleware conditionally to specific path
+app.UseWhen(config => config.Request.Path.StartsWithSegments("/api/game"),
+    gameApi =>
+    {
+        gameApi.UseMiddleware<ApiKeyMiddleware>();
 
-app.AddApiVersioning(config =>
+    });
+
+app.Map("/api/GetDateTime", async dtApi =>
 {
-    config.DefaultApiVersion = new ApiVersion(1, 0); // Set the default API version to 1.0 if a version is not specified in the request.
-    config.AssumeDefaultVersionWhenUnspecified = true; // Assume default version when not specified
-    config.ReportApiVersions = true; // To Let clients know which API versions are supported
-    config.ApiVersionReader = new HeaderApiVersionReader("api-version"); // To pass the version information in the request header.
+    await dtApi.Response.WriteAsJsonAsync(new
+    {
+        CurrentDateTime = DateTime.Now
+    });
 });
+
+app.MapWhen(context => context.Request.Path.StartsWithSegments("/api/DateTime"),
+    app1 => app1.Run(async context =>
+    {
+        await context.Response.WriteAsJsonAsync(new
+        {
+            CurrentDateTime = DateTime.Now,
+            message = "This endpoint provides the current date and time."
+        });
+    }));
 
 app.Run();
 // run db migrations and seed data
