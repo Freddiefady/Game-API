be673d3f1b0fba37faaaa2275c1dd3900ae2394f
Freddiefady <dodofady49@gmail.com>
2026-01-04 17:18:33 +0200

    Integrate Serilog for structured logging
    Replace default logging with Serilog, configuring it via appsettings.json.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using GameStore.Endpoints;
 using GameStore.Middleware;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,26 @@ builder.Services.AddApiVersioning(config =>
     config.ApiVersionReader = new HeaderApiVersionReader("api-version"); // To pass the version information in the request header.
 });
 
+// Configure Serilog
+//builder.Host.UseSerilog((context, config) =>
+//{
+//    config.MinimumLevel.Information()
+//        .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+//        .WriteTo.Debug(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {level:u3} {Message:lj}{NewLine}{Exception}")
+//        .WriteTo.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {level:u3} {Message:lj}{NewLine}{Exception}");
+//});
+
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+
+// If you want to use CorrelationId enrichment
+//builder.Host.UseSerilog((context, config) =>
+//{
+//    config.MinimumLevel.Information()
+//        .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+//        .Enrich.WithCorrelationId()
+//        .WriteTo.Debug(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {level:u3} {Message:lj}{NewLine}{Exception}")
+//        .WriteTo.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {level:u3} {Message:lj}{NewLine}{Exception}");
+//});
 
 builder.AddGameStoreDb();
 
@@ -46,13 +67,16 @@ app.Map("/api/GetDateTime", async dtApi =>
 app.MapWhen(context => context.Request.Path.StartsWithSegments("/api/DateTime"),
     app1 => app1.Run(async context =>
     {
+        Log.Information("Endpoint start");
         await context.Response.WriteAsJsonAsync(new
         {
             CurrentDateTime = DateTime.Now,
             message = "This endpoint provides the current date and time."
         });
+        Log.Information($"Endpoint finished {context.Response.StatusCode}");
     }));
 
+app.UseSerilogRequestLogging();
 app.Run();
 // run db migrations and seed data
 // first use namespaces in program.cs
